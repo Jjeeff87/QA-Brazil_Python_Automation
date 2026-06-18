c1abc87256f7748a926a2d13221c2a309ae9dfb3
jefersonschneider <j.ferson@icloud.com>
2026-06-18 11:09:59 +0100

    add server url

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-URBAN_ROUTES_URL = ""
+URBAN_ROUTES_URL = "https://cnt-ffd34c10-a1c3-4d90-a131-690e2d7fb945.containerhub.tripleten-services.com?lng=pt"
 ADDRESS_FROM = "East 2nd Street, 601"
 ADDRESS_TO = "1300 1st St"
 PHONE_NUMBER = "+1 123 123 12 12"
